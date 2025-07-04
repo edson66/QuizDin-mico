a0f65335274d69dd85edfd023f7823b9b4a578ab
Edson <edsonulissesr1@gmail.com>
2025-07-04 01:47:04 -0300

    Add files via upload
    implementação da função jogar ainda não completa

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <cctype>
+#include <sstream>
 
 using namespace std;
 
@@ -46,6 +47,88 @@ int main(){
     }while (true);
 
     return 0;
+}
+
+void jogar(){
+    string tema,dificuldade;
+    system("cls");
+    cout << "\nDigite a dificuldade que deseja(facil,medio ou dificil): ";
+    cin >> dificuldade;
+    if (dificuldade != "facil" && dificuldade != "medio" && dificuldade != "dificil"){
+         cout << "\nDificuldade invalida. Use: facil, medio ou dificil.\n";
+         return;
+    }
+    cout << "\nEscolha um ou mais destes temas de perguntas --> (portugues,matematica,";
+    cout << "\nesportes,historia,computacao ou ciencia) os separando entre espaços: ";
+    cin.ignore();
+    getline(cin,tema);
+
+    ifstream arquivo(tema + ".csv");
+    if (!arquivo.is_open()){
+        cout << "\nNao foi possivel abrir o arquivo" << tema << ".csv,tente novamente";
+        return;
+    }
+    int quantidade = 0;
+    string linha;
+    while(getline(arquivo,linha)){
+        if (linha.find(";" + dificuldade + ";") != string::npos){
+            quantidade++;
+        }
+    }
+    if(quantidade == 0){
+        cout<< "\nNao ha perguntas com essa dificuldade";
+        return;
+    }
+    if (quantidade > 0 && quantidade < 3) {
+        cout << "\nErro: eh necessario ter pelo menos 3 perguntas com essa dificuldade.\n";
+        return;
+    }
+    Pergunta* perguntas = new Pergunta[quantidade];
+    arquivo.clear();
+    arquivo.seekg(0);
+    int j = 0;
+    while(getline(arquivo,linha)){
+        if (linha.find(";" + dificuldade + ";") != string::npos){
+            stringstream ss(linha);
+            getline(ss,perguntas[j].tema,';');
+            getline(ss,perguntas[j].dificuldade,';');
+            getline(ss,perguntas[j].pergunta,';');
+            getline(ss,perguntas[j].opcaoA,';');
+            getline(ss,perguntas[j].opcaoB,';');
+            getline(ss,perguntas[j].opcaoC,';');
+            ss >> perguntas[j].resposta_correta;
+            ss.ignore();
+            ss >> perguntas[j].pontuacao;
+            j++;
+        }
+    }
+    arquivo.close();
+    int pontuacao = 0;
+    char resposta;
+    system("cls");
+    cout << "Rodada de 3 perguntas . . .\n";
+    for(int i=0;i<3;i++){
+        cout << "Pergunta " << i+1 <<": "<<perguntas[i].pergunta << "\n";
+        cout << "A- " << perguntas[i].opcaoA << "\n";
+        cout << "B- " << perguntas[i].opcaoB << "\n";
+        cout << "C- " << perguntas[i].opcaoC << "\n";
+        cout << "Digite sua resposta: ";
+        cin >> resposta;
+        resposta = tolower(resposta);
+        if(resposta == perguntas[i].resposta_correta){
+            cout << "\nResposta Correta!! + "<< perguntas[i].pontuacao << "pontos\n";
+            pontuacao += perguntas[i].pontuacao;
+            system("pause");
+            system("cls");
+        } 
+        else{
+            cout << "\nNao foi dessa vez, Resposta correta : Letra " << perguntas[i].resposta_correta << "\n";
+            system("pause");
+            system("cls");
+        }
+    }
+    cout << "\nPontuacao total : " << pontuacao << "\n";
+    delete[] perguntas;
 }
 
 void adicionar_pergunta(){
