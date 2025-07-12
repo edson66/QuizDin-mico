6038b46171ec160ea882c8e040765d2f99f28534
Cuzcuzeiro <lucaslimase98399@gmail.com>
2025-07-12 20:19:25 -0300

    Add files via upload
    Adição de Rankings

--- tudo.cpp
+++ tudo.cpp
@@ -19,11 +19,17 @@ char resposta_correta;
 int pontuacao;
 };
 
+struct jogador{
+    string nome;
+    int pontuacao;
+};
+
 void adicionar_pergunta();
 void exibir_menu(int& escolha);
 void jogar();
 void exibir_regras();
-//void mostrar_ranking();
+void mostrar_ranking();
+void salvar_ranking(string nome,int pontuacao);
 void salvar_no_arquivo(string nome_arquivo,Pergunta p);
 
 int main(){
@@ -36,7 +42,7 @@ int main(){
      case 2:
      exibir_regras(); break;
      case 3:
-      //mostrar_ranking(); break;
+      mostrar_ranking(); break;
      case 4:
       adicionar_pergunta();
       system("pause");
@@ -53,7 +59,7 @@ int main(){
 
 void jogar(){
     srand(time(0));
-    string tema,dificuldade;
+    string tema,dificuldade,nome;
     system("cls");
     cout << "\nDigite a dificuldade que deseja(facil,medio ou dificil): ";
     cin >> dificuldade;
@@ -159,8 +165,9 @@ void jogar(){
         }
     }
     cout << "\nPontuacao total : " << pontuacao;
-    cout << "\npressione enter para continuar";
-    cin.ignore();
+    cout << "\nDigite seu nome para salvar no ranking: ";
+    getline(cin,nome);
+    salvar_ranking(nome,pontuacao);
     system("cls");
  
     delete[] perguntas;
@@ -288,3 +295,88 @@ void exibir_regras(){
     cin.ignore();
     cin.get();
 } 
+
+void salvar_ranking(string nome,int pontuacao){
+    ifstream arquivo_ler("ranking.csv");
+
+    string linha;
+    int quant = 0;
+    while (getline(arquivo_ler, linha)) {
+        quant++;
+    }
+
+    arquivo_ler.clear();
+    arquivo_ler.seekg(0);
+    jogador* jogadores = new jogador[quant + 1]; 
+    int i = 0;
+    bool existe = false;
+
+    while (getline(arquivo_ler, linha)) {
+        stringstream ss(linha);
+        getline(ss, jogadores[i].nome, ';');
+        ss >> jogadores[i].pontuacao;
+
+        if (jogadores[i].nome == nome) {
+            jogadores[i].pontuacao += pontuacao;
+            existe = true;
+        }
+        i++;
+    }
+    arquivo_ler.close();
+
+    if (!existe) {
+        jogadores[i].nome = nome;
+        jogadores[i].pontuacao = pontuacao;
+        i++;
+    }
+
+    ofstream arquivo("ranking.csv");
+    if(!arquivo.is_open()){
+        cout << "erro ao abrir o arquivo de ranking.";
+        return;
+    }
+    for (int j = 0; j < i; j++) {
+        arquivo << jogadores[j].nome << ";" << jogadores[j].pontuacao << "\n";
+    }
+    arquivo.close();
+    delete[] jogadores;
+    return;
+}
+
+void mostrar_ranking(){
+    ifstream arquivo("ranking.csv");
+
+    string linha;
+    int quant = 0;
+    while(getline(arquivo,linha)){
+        quant ++;
+    }
+    if (quant == 0) {
+    cout << "\nRanking vazio.Pressione enter para voltar ao menu . .\n";
+    cin.ignore();
+    cin.get();
+    return;
+    }
+    jogador* jogadores = new jogador[quant];
+    arquivo.clear();
+    arquivo.seekg(0);
+    int i = 0;
+    while(getline(arquivo,linha)){
+        stringstream ss (linha);
+        getline(ss,jogadores[i].nome,';');
+        ss >> jogadores[i].pontuacao;
+        i++;
+    }
+    arquivo.close();
+
+    cout << "\n===== RANKING =====\n";
+    for(int i=0;i<quant;i++){
+        cout << "Nome :" << jogadores[i].nome << "\nPontuacao:" << jogadores[i].pontuacao << "\n-------------------\n";
+    }
+
+    delete[] jogadores;
+    cout << "\nPressione enter para voltar ao menu . . ";
+    cin.ignore();
+    cin.get();
+
+}
