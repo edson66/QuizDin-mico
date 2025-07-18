d58ab869a5f2d9b25e36c0a65dbc81f3459063ca
Edson Ulisses <edsonulissesr1@gmail.com>
2025-07-18 15:53:15 -0300

    versão final
    adição de escolha de 1 ou 2 temas + perda de pontos

--- jogar.cpp
+++ jogar.cpp
@@ -7,12 +7,13 @@
 #include <ctime>
 #include "pergunta.h"
 #include "jogo.h"
+#include "ranking.h"
 
 using namespace std;
 
 void jogar(){
     srand(time(0));
-    string tema,dificuldade;
+    string temas,tema1,tema2,dificuldade,nome;
     system("cls");
     cout << "\nDigite a dificuldade que deseja(facil,medio ou dificil): ";
     cin >> dificuldade;
@@ -20,25 +21,38 @@ void jogar(){
          cout << "\nDificuldade invalida. Use: facil, medio ou dificil.\n";
          return;
     }
-    cout << "\nEscolha um ou mais destes temas de perguntas --> (portugues,matematica,";
+    cout << "\nEscolha um ou dois destes temas de perguntas --> (portugues,matematica,";
     cout << "\nesportes,historia,computacao ou ciencia) os separando entre espacos: ";
     cin.ignore();
-    getline(cin,tema);
+    getline(cin,temas);
+    stringstream ss(temas);
+    ss >> tema1 >> tema2;
 
-    ifstream arquivo(tema + ".csv");
-    if (!arquivo.is_open()){
-        cout << "\nNao foi possivel abrir o arquivo" << tema << ".csv,tente novamente";
+    ifstream arquivo1(tema1 + ".csv");
+    ifstream arquivo2;
+    if (!tema2.empty()){
+        arquivo2.open(tema2 + ".csv");
+    }
+    if (!arquivo1.is_open() || (!tema2.empty() && !arquivo2.is_open())){
+        cout << "\nNao foi possivel abrir um dos arquivos.\n";
         return;
     }
     int quantidade = 0;
     string linha;
-    while(getline(arquivo,linha)){
+    while(getline(arquivo1,linha)){
         if (linha.find(";" + dificuldade + ";") != string::npos){
             quantidade++;
         }
     }
+    if(!tema2.empty()) {  
+        while(getline(arquivo2,linha)){
+            if (linha.find(";" + dificuldade + ";") != string::npos){
+                quantidade++;
+            }
+        }
+    }
     if(quantidade == 0){
-        cout<< "\nNao ha perguntas com essa dificuldade";
+        cout<< "\nNao ha perguntas com essa(s) dificuldade(s)";
         return;
     }
     if (quantidade > 0 && quantidade < 3) {
@@ -46,10 +60,12 @@ void jogar(){
         return;
     }
     Pergunta* perguntas = new Pergunta[quantidade];
-    arquivo.clear();
-    arquivo.seekg(0);
+    arquivo1.clear();
+    arquivo1.seekg(0);
+    arquivo2.clear();
+    arquivo2.seekg(0);
     int j = 0;
-    while(getline(arquivo,linha)){
+    while(getline(arquivo1,linha)){
         if (linha.find(";" + dificuldade + ";") != string::npos){
             stringstream ss(linha);
             getline(ss,perguntas[j].tema,';');
@@ -64,18 +80,33 @@ void jogar(){
             j++;
         }
     }
-    arquivo.close();
+    if(!tema2.empty()){
+        while(getline(arquivo2,linha)){
+            if (linha.find(";" + dificuldade + ";") != string::npos){
+                stringstream ss(linha);
+                getline(ss,perguntas[j].tema,';');
+                getline(ss,perguntas[j].dificuldade,';');
+                getline(ss,perguntas[j].pergunta,';');
+                getline(ss,perguntas[j].opcaoA,';');
+                getline(ss,perguntas[j].opcaoB,';');
+                getline(ss,perguntas[j].opcaoC,';');
+                ss >> perguntas[j].resposta_correta;
+                ss.ignore();
+                ss >> perguntas[j].pontuacao;
+                j++;
+            }
+        }        
+    }
+    arquivo1.close();
+    arquivo2.close();
     int pontuacao = 0;
     char resposta;
     system("cls");
-    //inicio
     int indices[3];
     int sorteados = 0;
 
     while (sorteados < 3) {
     int aleatorio = rand() % quantidade;
-
-    // verificar se já foi sorteado
     bool repetido = false;
     for (int j = 0; j < sorteados; j++) {
         if (indices[j] == aleatorio) {
@@ -89,7 +120,6 @@ void jogar(){
         sorteados++;
     }
 }
-//fim da intrusão
     cout << "Rodada de 3 perguntas . . .\n";
     for(int i=0;i<3;i++){
         int rand=indices[i];
@@ -100,16 +130,18 @@ void jogar(){
         cout << "Digite sua resposta: ";
         cin >> resposta;
         resposta = tolower(resposta);
-        if(resposta == perguntas[i].resposta_correta){
-            cout << "\nResposta Correta!! + "<< perguntas[i].pontuacao << "pontos\n";
-            pontuacao += perguntas[i].pontuacao;
+        if(resposta == perguntas[rand].resposta_correta){
+            cout << "\nResposta Correta!! + "<< perguntas[rand].pontuacao << "pontos\n";
+            pontuacao += perguntas[rand].pontuacao;
             cout << "pressione enter para continuar";
             cin.ignore();
             cin.get();
             system("cls");
         } 
         else{
-            cout << "\nNao foi dessa vez, Resposta correta : Letra " << perguntas[i].resposta_correta << "\n";
+            cout << "\nNao foi dessa vez, Resposta correta : Letra " << perguntas[rand].resposta_correta << "\n";
+            cout << "-" << perguntas[rand].pontuacao << "pontos\n";
+            pontuacao -= perguntas[rand].pontuacao;
             cout << "pressione enter para continuar";
             cin.ignore();
             cin.get();
@@ -118,8 +150,9 @@ void jogar(){
         }
     }
     cout << "\nPontuacao total : " << pontuacao;
-    cout << "\npressione enter para continuar";
-    cin.ignore();
+    cout << "\nDigite seu nome para salvar no ranking: ";
+    getline(cin,nome);
+    salvar_ranking(nome,pontuacao);
     system("cls");
  
     delete[] perguntas;
